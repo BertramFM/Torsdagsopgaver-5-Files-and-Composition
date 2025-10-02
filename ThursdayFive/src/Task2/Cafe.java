2537949745c26f0c0e3f02f2e103bc041f8bfbbe
BertramFM <160260021+BertramFM@users.noreply.github.com>
2025-10-02 22:09:14 +0200

    task2

--- ThursdayFive/src/Task2/Cafe.java
+++ ThursdayFive/src/Task2/Cafe.java
@@ -1,4 +1,30 @@
 package Task2;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Cafe {
+    private ArrayList<String> coffeMenu = new ArrayList<>();
+
+    public ArrayList<String> getCoffeMenu() {
+        return coffeMenu;
+    }
+
+    public void loadMenuData() {
+        File file = new File("Coffees.txt");
+
+        try {
+            Scanner sc = new Scanner(file);
+
+            while(sc.hasNextLine()) {
+                String coffe = sc.nextLine();
+                coffeMenu.add(coffe);
+            }
+            sc.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
 }
--- ThursdayFive/src/Task2/Main.java
+++ ThursdayFive/src/Task2/Main.java
@@ -1,8 +1,17 @@
 package Task2;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        Cafe cafe = new  Cafe();
+        cafe.loadMenuData();
+        cafe.getCoffeMenu();
 
+        ArrayList<String> coffeMenu = cafe.getCoffeMenu();
+        for (String coffe: coffeMenu) {
+            System.out.println(coffe);
+        }
     }
 }
